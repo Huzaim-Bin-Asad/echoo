253a898b2b888f2c59e7be27f63694af46d876b1
HUZAIM-BIN-ASAD <huzaimbinasad@gmail.com>
2025-04-20 09:31:57 +0500

    Redesigned Settings/Chat(Main)

--- src/components/Settings/Chats/main/ArchivedSection.jsx
+++ src/components/Settings/Chats/main/ArchivedSection.jsx
@@ -2,10 +2,20 @@ import React from "react";
 
 const ArchivedSection = () => {
   return (
-    <div className="mb-4">
-      <p className="fw-bold">Archived chats</p>
+    <div className="mb-4 border-bottom border-secondary-subtle pb-3 mt-3">
+      <p className="fw-semibold text-secondary" style={{ marginLeft: "25px" }}>Archived chats</p>
       <div className="ps-3">
-        <p>Keep chats archived <span className="text-muted">Archived chats will remain archived when you receive a new message</span></p>
+        <div className="d-flex justify-content-between align-items-start mb-2 ms-4">
+          <div>
+            <p className="mb-1 fs-6">Keep chats archived</p>
+            <small className="text-secondary">
+              Archived chats will remain archived when you receive a new message
+            </small>
+          </div>
+          <div className="form-check form-switch" style={{ transform: "scale(1.4)", marginRight: "8px", marginTop: "20px" }}>
+            <input className="form-check-input" type="checkbox" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Settings/Chats/main/ChatGeneralSection.jsx
+++ src/components/Settings/Chats/main/ChatGeneralSection.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { FolderCode, History } from "lucide-react";
 
 const ChatGeneralSection = () => {
   return (
-    <div className="mb-4">
-      <p className="fw-bold">Chat General</p>
+    <div className="mb-3 mt-3">
+      <p
+        className="fw-semibold text-secondary"
+        style={{ marginLeft: "16px", fontSize: "0.95rem" }}
+      >
+        Chat General
+      </p>
       <div className="ps-3">
-        <p>Option 1</p>
-        <p>Option 2</p>
+        <div className="d-flex align-items-start mb-4">
+          <FolderCode size={24} className="me-2 mt-1" />
+          <div className="ms-1">
+            <p className="mb-1" style={{ fontSize: "1rem" }}>Chat Backup</p>
+            <small className="text-secondary" style={{ fontSize: "0.75rem" }}>
+              Backup your chats to the cloud
+            </small>
+          </div>
+        </div>
+
+        <div className="d-flex align-items-start mb-2">
+          <History size={22} className="me-3 mt-1" />
+          <p className="mb-0" style={{ fontSize: "1rem" }}>Chat History</p>
+        </div>
       </div>
     </div>
   );
